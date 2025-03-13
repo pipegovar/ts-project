2163aee27ec78ebfbb0cc555af2fad6cce8812dc
Felipe Gomez <luisfelipe_1002@hotmail.com>
2025-03-12 21:31:06 -0500

    Módulos: import y export

---
+++ src/products/main.ts
@@ -0,0 +1,20 @@
+import { createProduct, calcStock, products } from './product.service';
+
+
+createProduct({
+    name: 'Prod1',
+    createdAt: new Date(2001, 2, 10),
+    stock: 6
+})
+createProduct({
+    name: 'Prod1',
+    createdAt: new Date(2001, 2, 10),
+    stock: 10,
+    size: 'L'
+})
+
+console.log(products);
+const total = calcStock();
+console.log(total);
+
+
